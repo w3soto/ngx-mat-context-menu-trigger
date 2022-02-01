a532339cd4e8c6cc45b39341c9f26890fe66faf2
w3soto <soto.ms@gmail.com>
2022-02-01 15:17:44 +0100

    Add tests

--- projects/ngx-mat-context-menu-trigger/src/lib/ngx-mat-context-menu-trigger-holder.component.spec.ts
+++ projects/ngx-mat-context-menu-trigger/src/lib/ngx-mat-context-menu-trigger-holder.component.spec.ts
@@ -1,25 +1,94 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, flush, TestBed, tick } from '@angular/core/testing';
+import { MatMenu, MatMenuModule } from "@angular/material/menu";
+import { Component, ViewChild } from "@angular/core";
+import { By } from "@angular/platform-browser";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+
 import { NgxMatContextMenuTriggerHolder } from './ngx-mat-context-menu-trigger-holder.component';
 
 
+@Component({
+  selector: 'test-component',
+  template: `
+    <ngx-mat-context-menu-trigger-holder
+      [menu]="testMenu"
+      [menuData]="{text: text}"      
+    ></ngx-mat-context-menu-trigger-holder>
+    <mat-menu #testMenu>
+      <ng-template matMenuContent let-text="text">
+        <button mat-menu-item>{{ text }}</button>
+      </ng-template>
+    </mat-menu>
+  `
+})
+class TestComponent {
+  text: string = 'Hello World';
+  @ViewChild(NgxMatContextMenuTriggerHolder, {static: true})
+  holder!: NgxMatContextMenuTriggerHolder;
+  @ViewChild(MatMenu, {static: true})
+  menu!: MatMenu
+}
+
+
 describe('NgxMatContextMenuTriggerHolder', () => {
-  let component: NgxMatContextMenuTriggerHolder;
-  let fixture: ComponentFixture<NgxMatContextMenuTriggerHolder>;
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NgxMatContextMenuTriggerHolder ]
+      declarations: [
+        TestComponent,
+        NgxMatContextMenuTriggerHolder
+      ],
+      imports: [
+        NoopAnimationsModule,
+        MatMenuModule,
+      ]
     })
     .compileComponents();
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(NgxMatContextMenuTriggerHolder);
-    component = fixture.componentInstance;
+  beforeEach(async () => {
+    fixture = TestBed.createComponent(TestComponent);
     fixture.detectChanges();
+    component = fixture.componentInstance;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(component.holder).toBeTruthy();
+    expect(component.menu).toBeTruthy();
   });
+
+  it('should open and close menu', fakeAsync(() => {
+    component.holder.openMenu(0,0);
+    let openMenuItem = fixture.debugElement.query(By.css(".mat-menu-item"));
+    expect(openMenuItem).toBeTruthy();
+
+    tick(100);
+
+    component.holder.closeMenu();
+    let closeMenuItem = fixture.debugElement.query(By.css(".mat-menu-item"));
+    expect(closeMenuItem).toBeFalsy();
+
+    flush();
+  }));
+
+  it('should pass data to menu', fakeAsync(() => {
+    const text = 'Second World';
+
+    component.text = text;
+    fixture.detectChanges();
+
+    component.holder.openMenu(0,0);
+    fixture.detectChanges();
+
+    tick(100);
+
+    const menuItem = fixture.debugElement.query(By.css(".mat-menu-item"));
+    expect(menuItem.nativeElement.textContent).toEqual(text);
+
+    flush();
+  }));
+
 });
--- projects/ngx-mat-context-menu-trigger/src/lib/ngx-mat-context-menu-trigger.directive.spec.ts
+++ projects/ngx-mat-context-menu-trigger/src/lib/ngx-mat-context-menu-trigger.directive.spec.ts
@@ -1,20 +1,59 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, flush, TestBed, tick } from '@angular/core/testing';
+import { Component, ViewChild } from "@angular/core";
+import { MatMenu, MatMenuModule } from "@angular/material/menu";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { By } from "@angular/platform-browser";
 
 import { NgxMatContextMenuTrigger } from './ngx-mat-context-menu-trigger.directive';
+import { NgxMatContextMenuTriggerModule } from "./ngx-mat-context-menu-trigger.module";
+
+
+@Component({
+  selector: 'test-component',
+  template: `
+    <div 
+      [ngxMatContextMenuTriggerFor]="testMenu"
+      [ngxMatContextMenuTriggerData]="{text: text}"  
+      #trigger="ngxMatContextMenuTrigger">
+    </div>
+    <mat-menu #testMenu>
+      <ng-template matMenuContent let-text="text">
+        <button mat-menu-item>{{ text }}</button>
+      </ng-template>
+    </mat-menu>
+  `
+})
+class TestComponent {
+  text: string = 'Hello World';
+  @ViewChild('trigger', {static: true})
+  trigger!: NgxMatContextMenuTrigger;
+  @ViewChild(MatMenu, {static: true})
+  menu!: MatMenu
+}
+
 
 describe('NgxMatContextMenuTrigger', () => {
-  let component: NgxMatContextMenuTrigger;
-  let fixture: ComponentFixture<NgxMatContextMenuTrigger>;
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NgxMatContextMenuTrigger ]
+      declarations: [
+        TestComponent,
+        NgxMatContextMenuTrigger
+      ],
+      imports: [
+        NoopAnimationsModule,
+        MatMenuModule,
+        // must import module due to "Export of name 'matMenuTrigger' not found!" error...
+        NgxMatContextMenuTriggerModule
+      ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NgxMatContextMenuTrigger);
+    fixture = TestBed.createComponent(TestComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -22,4 +61,40 @@ describe('NgxMatContextMenuTrigger', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should open and close menu', fakeAsync(() => {
+    component.trigger.openMenu(0,0);
+    fixture.detectChanges();
+
+    tick(100);
+
+    let openMenuItem = fixture.debugElement.query(By.css(".mat-menu-item"));
+    expect(openMenuItem).toBeTruthy();
+
+    tick(100);
+
+    component.trigger.closeMenu();
+    let closeMenuItem = fixture.debugElement.query(By.css(".mat-menu-item"));
+    expect(closeMenuItem).toBeFalsy();
+
+    flush();
+  }));
+
+  it('should pass data to menu', fakeAsync(() => {
+    const text = 'Second World';
+
+    component.text = text;
+    fixture.detectChanges();
+
+    component.trigger.openMenu(0,0);
+    fixture.detectChanges();
+
+    tick(100);
+
+    const menuItem = fixture.debugElement.query(By.css(".mat-menu-item"));
+    expect(menuItem.nativeElement.textContent).toEqual(text);
+
+    flush();
+  }));
+
 });
