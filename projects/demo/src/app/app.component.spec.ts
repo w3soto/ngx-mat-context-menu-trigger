615267e294ed00463b8b4515e8641b45649d5631
w3soto <soto.ms@gmail.com>
2022-02-03 18:11:37 +0100

    Fix test in demo app

--- projects/demo/src/app/app.component.spec.ts
+++ projects/demo/src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { MatMenuModule } from "@angular/material/menu";
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -7,6 +8,9 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [
+        MatMenuModule
+      ]
     }).compileComponents();
   });
 
